684d25fb54e13ab268299b7da83758322533fc45
Fauzan <fauzantezasaputra@gmail.com>
2026-06-21 01:43:37 +0700

    add Vercel-compatible entry point with reconfigured storage paths and debug settings

--- api/index.php
+++ api/index.php
@@ -27,6 +27,23 @@ putenv('APP_ENV=local');
 $_ENV['APP_DEBUG'] = 'true';
 $_ENV['APP_ENV'] = 'local';
 
+// Redirect all Laravel caches to the writable /tmp directory
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/bootstrap/cache/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/storage/bootstrap/cache/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/bootstrap/cache/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/bootstrap/cache/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/bootstrap/cache/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+putenv('APP_CONFIG_CACHE=' . $_ENV['APP_CONFIG_CACHE']);
+putenv('APP_EVENTS_CACHE=' . $_ENV['APP_EVENTS_CACHE']);
+putenv('APP_PACKAGES_CACHE=' . $_ENV['APP_PACKAGES_CACHE']);
+putenv('APP_ROUTES_CACHE=' . $_ENV['APP_ROUTES_CACHE']);
+putenv('APP_SERVICES_CACHE=' . $_ENV['APP_SERVICES_CACHE']);
+putenv('VIEW_COMPILED_PATH=' . $_ENV['VIEW_COMPILED_PATH']);
+
+@mkdir('/tmp/storage/bootstrap/cache', 0777, true);
+
 require __DIR__.'/../vendor/autoload.php';
 
 /** @var Application $app */
