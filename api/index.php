9a8c2127ab733461dc41419451418c31358af238
Fauzan <fauzantezasaputra@gmail.com>
2026-06-21 01:34:52 +0700

    configure storage path to use /tmp directory for Vercel deployment compatibility

--- api/index.php
+++ api/index.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Override Laravel's storage paths to use the writable /tmp directory in Vercel
 $storage = '/tmp/storage';
 if (!is_dir($storage)) {
     mkdir($storage, 0777, true);
@@ -11,7 +17,14 @@ if (!is_dir($storage)) {
     mkdir($storage . '/logs', 0777, true);
 }
 
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 $app->useStoragePath($storage);
 
-require __DIR__ . '/../public/index.php';
+$app->handleRequest(Request::capture());
