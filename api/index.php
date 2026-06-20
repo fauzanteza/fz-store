9446b426a82ce1b973b02bccae075864d10c52be
Fauzan <fauzantezasaputra@gmail.com>
2026-06-21 01:31:38 +0700

    initialize custom storage path in API entry point

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
+$storage = '/tmp/storage';
+if (!is_dir($storage)) {
+    mkdir($storage, 0777, true);
+    mkdir($storage . '/app', 0777, true);
+    mkdir($storage . '/framework', 0777, true);
+    mkdir($storage . '/framework/cache', 0777, true);
+    mkdir($storage . '/framework/sessions', 0777, true);
+    mkdir($storage . '/framework/views', 0777, true);
+    mkdir($storage . '/logs', 0777, true);
+}
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$app->useStoragePath($storage);
+
 require __DIR__ . '/../public/index.php';
